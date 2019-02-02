5b8a03f7ecbba5b96f13f2300b3b9cc0dd8e567e
Indip <indipniroula@gmail.com>
2019-02-02 02:57:35 +0000

    LottoNum completed

--- introExercises/src/RecapCode/LottoNum.java
+++ introExercises/src/RecapCode/LottoNum.java
@@ -1,7 +1,9 @@
 package RecapCode;
 // Git code: 4c70
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
 public class LottoNum {
@@ -13,12 +15,23 @@ public class LottoNum {
     public static void lotto(){
         Random generator = new Random();
         final int TOTAL_NUMS = 49;
-        int[] lottoNums = new int[7];
-        for (int i = 0; i < lottoNums.length; i++) {
-            lottoNums[i] = generator.nextInt(TOTAL_NUMS);
+        final int TOTAL_BALLS = 7;
+        List<Integer> lottoNums = new ArrayList<>();
+        Integer num = generator.nextInt(TOTAL_NUMS) + 1;
+        for (int i = 1; i < TOTAL_BALLS; i++) {
+            while (lottoNums.contains(num)) {
+                num = generator.nextInt(TOTAL_NUMS) + 1;
+            }
+            lottoNums.add(num);
+            System.out.println("Number " + i + " : " + num);
         }
 
-        System.out.println(Arrays.toString(lottoNums));
+        //Bonus Num
+        while (lottoNums.contains(num)) {
+            num = generator.nextInt(TOTAL_NUMS) + 1;
+        }
+        System.out.println("Bonus Number: " + num);
+
 
     }
 }
